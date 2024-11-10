fdc04b60dcfcc71412832e87f76d3cd0ea952f4b
Alex Davis <a_davis47@u.pacific.edu>
2024-11-09 17:43:35 -0800

    Added deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ export default defineConfig({
     server: {
         port: 3000
     },
-    assetsInclude: ['**/*.glsl']
+    assetsInclude: ['**/*.glsl'],
+    base: "computergraphics-assignment6"
 });
